c944f6723abd67168f70de1fe2c25917c54053d4
Eats <hibrod22@gmail.com>
2023-04-25 13:51:55 -0700

    Test.

---
+++ assets/js/categories.js
@@ -0,0 +1,35 @@
+---
+---
+
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+
+console.log(categories);
+
+window.onload = function () {
+  document.querySelectorAll(".article-category").forEach((category) => {
+    category.addEventListener("click", function (e) {
+      const posts = categories[e.target.innerText.replace(" ","_")];
+      console.log(posts);
+      let html = ``
+      posts.forEach(post=>{
+        html += `
+        <a class="modal-article" href="${post.url}">
+          <h4>${post.title}</h4>
+          <small class="modal-article-date">${post.date}</small>
+        </a>
+        `
+      })
+      document.querySelector("#category-modal-title").innerText = e.target.innerText;
+      document.querySelector("#category-modal-content").innerHTML = html;
+      document.querySelector("#category-modal-bg").classList.toggle("open");
+      document.querySelector("#category-modal").classList.toggle("open");
+    });
+  });
+
+  document.querySelector("#category-modal-bg").addEventListener("click", function(){
+    document.querySelector("#category-modal-title").innerText = "";
+    document.querySelector("#category-modal-content").innerHTML = "";
+    document.querySelector("#category-modal-bg").classList.toggle("open");
+    document.querySelector("#category-modal").classList.toggle("open");
+  })
+};
